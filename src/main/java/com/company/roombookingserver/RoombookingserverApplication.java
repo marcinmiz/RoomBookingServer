6e404ee6beb5b75a0cd10e228d19d339114440f5
Marcin Mizgała <maro181114@gmail.com>
2020-07-17 13:50:31 +0200

    Initial commit

---
+++ src/main/java/com/company/roombookingserver/RoombookingserverApplication.java
@@ -0,0 +1,13 @@
+package com.company.roombookingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombookingserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoombookingserverApplication.class, args);
+    }
+
+}
